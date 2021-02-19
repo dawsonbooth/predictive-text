939132bdec305e6f922b85e30cb14f35b0b8c5cc
dawsonbooth <github@dawsonbooth.com>
2021-02-18 23:06:37 -0600

    Extract nlp module

---
+++ src/nlp.py
@@ -0,0 +1,14 @@
+import re
+from typing import Generator, List, Sequence, Tuple
+
+
+def ngrams(tokens: Sequence[str], n: int) -> Generator[Tuple[str, ...], None, None]:
+    for i in range(len(tokens) - n + 1):
+        yield tuple(tokens[i : i + n])
+
+
+def tokenize(text: str) -> List[str]:
+    text = re.sub(r"\d", "", text)
+    text = re.sub(r"[^\w\s]", " ", text)
+
+    return text.split()
